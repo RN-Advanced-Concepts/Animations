3bbf62a1aeea1be1628c0fafc51b4f3950b90c2d
fotis tsakiris <footios76@gmail.com>
2020-01-26 21:32:23 +0200

    array of cards added + drug 1st one

--- src/Ball.js
+++ src/Ball.js
@@ -3,14 +3,17 @@ import { View, StyleSheet, Animated } from 'react-native';
 
 class Ball extends Component {
 	UNSAFE_componentWillMount() {
-		this.position = new Animated.ValueXY(0, 0);
+        this.position = new Animated.ValueXY(0, 0); // 1. current position of the element
+        // 2. code that changes the current position
 		Animated.spring(this.position, {
 			toValue: { x: 200, y: 500 }
 		}).start();
 	}
 	render() {
 		return (
-			<Animated.View style={this.position.getLayout()}>
+            // getLayout() - Converts {x, y} into {left, top} 
+            // 3. the actuall element that is going to be changed.
+			<Animated.View style={this.position.getLayout()}> 
 				<View style={styles.ball} />
 			</Animated.View>
 		);
---
+++ src/Deck.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
+
+class Deck extends Component {
+	constructor(props) {
+		super(props);
+
+		const position = new Animated.ValueXY();
+		const panResponder = PanResponder.create({
+			// If user tabs on the screen
+			onStartShouldSetPanResponder: () => true,
+			// If user drags the card
+			onPanResponderMove: (event, gesture) => {
+				// debugger
+				// console.log('gesture', gesture);
+				position.setValue({ x: gesture.dx, y: gesture.dy });
+			},
+			onPanResponderRelease: () => {}
+		});
+
+		this.state = { panResponder, position };
+    }
+    
+
+	renderCards() {
+		return this.props.data.map((item, index) => {
+			if (index === 0) {
+				return (
+					<Animated.View
+						key={item.id}
+						style={this.state.position.getLayout()}
+						{...this.state.panResponder.panHandlers}
+					>
+						{this.props.renderCard(item)}
+					</Animated.View>
+				);
+			}
+			return this.props.renderCard(item);
+		});
+	}
+	render() {
+		return <View>{this.renderCards()}</View>;
+	}
+}
+
+export default Deck;
