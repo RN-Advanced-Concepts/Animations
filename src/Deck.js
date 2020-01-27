0613b3c8d9bd3cd6d2384da366ca7895fde27bb4
fotis tsakiris <footios76@gmail.com>
2020-01-27 20:36:51 +0200

    onSwipeComplete

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+      }
+
 	constructor(props) {
 		super(props);
 
@@ -16,15 +21,17 @@ class Deck extends Component {
 			// If user drags the card
 			onPanResponderMove: (event, gesture) => {
 				// debugger
-				// console.log('gesture', gesture);
+                // console.log('gesture', gesture);
+                // move the card according to the move of the finger
 				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
 			onPanResponderRelease: (event, gesture) => {
 				if (gesture.dx > SWIPE_THRESHOLD) {
 					console.log('gesture.dx swipe right', gesture.dx);
-					this.forceSwipeRight();
+					this.forceSwipe('right');
 				} else if (gesture.dx < -SWIPE_THRESHOLD) {
-					console.log('gesture.dx swipe left', gesture.dx);
+                    console.log('gesture.dx swipe left', gesture.dx);
+					this.forceSwipe('left');
 				} else {
 					console.log('gesture.dx back to default', gesture.dx);
 					this.resetPosition();
@@ -32,14 +39,25 @@ class Deck extends Component {
 			}
 		});
 
-		this.state = { panResponder, position };
+        // panResponder does not call the state system
+		this.state = { panResponder, position, index: 0 };
 	}
 
-	forceSwipeRight() {
+	forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH + 100 : -SCREEN_WIDTH - 100
+        // timing is more simple than spring. 
+        // It just moves the item.
 		Animated.timing(this.state.position, {
-			toValue: { x: SCREEN_WIDTH + 100, y: 0 },
+			toValue: { x, y: 0 },
 			duration: SWIPE_OUT_DURATION
-		}).start();
+		}).start(() => this.onSwipeComplete(direction)); // onSwipeComplete is callback exe after start()
+	}
+
+	onSwipeComplete(direction) {
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index]
+
+		direction = 'right' ? onSwipeRight(item) : onSwipeLeft(item);
 	}
 
 	resetPosition() {
@@ -51,11 +69,18 @@ class Deck extends Component {
 	getCardStyle() {
 		const { position } = this.state;
 		const rotate = position.x.interpolate({
+            // * 2.5 = ... to have less rotation, because it take more distance to get to 120deg
 			inputRange: [ -SCREEN_WIDTH * 2.5, 0, SCREEN_WIDTH * 2.5 ],
 			outputRange: [ '-120deg', '0deg', '120deg' ]
 		});
 
 		return {
+            // ...position.getLayout() =
+            // reference the position object, get the current layout, 
+            // and pass it to the Animated.View, which takes it over from there 
+            // Note: here we need the spead operator, because we're returning a single objece 
+            // return {}
+            // so we need to add all the properties of getLayout to that object.
 			...position.getLayout(),
 			transform: [ { rotate } ]
 		};
@@ -65,6 +90,8 @@ class Deck extends Component {
 		return this.props.data.map((item, index) => {
 			if (index === 0) {
 				return (
+                    // {...this.state.panResponder.panHandlers} connects PanResponder to the View
+                    // style={this.props.position.getLayout()} // No spread operator needed!!!
 					<Animated.View key={item.id} style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
 						{this.props.renderCard(item)}
 					</Animated.View>
