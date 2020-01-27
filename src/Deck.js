33c6a23c2cecddb75621f4832a30a989d2b589f9
fotis tsakiris <footios76@gmail.com>
2020-01-27 08:33:33 +0200

    swipe card

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Dimensions, Animated, PanResponder } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
 	constructor(props) {
@@ -15,22 +19,53 @@ class Deck extends Component {
 				// console.log('gesture', gesture);
 				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
-			onPanResponderRelease: () => {}
+			onPanResponderRelease: (event, gesture) => {
+				if (gesture.dx > SWIPE_THRESHOLD) {
+					console.log('gesture.dx swipe right', gesture.dx);
+					this.forceSwipeRight();
+				} else if (gesture.dx < -SWIPE_THRESHOLD) {
+					console.log('gesture.dx swipe left', gesture.dx);
+				} else {
+					console.log('gesture.dx back to default', gesture.dx);
+					this.resetPosition();
+				}
+			}
 		});
 
 		this.state = { panResponder, position };
-    }
-    
+	}
+
+	forceSwipeRight() {
+		Animated.timing(this.state.position, {
+			toValue: { x: SCREEN_WIDTH + 100, y: 0 },
+			duration: SWIPE_OUT_DURATION
+		}).start();
+	}
+
+	resetPosition() {
+		Animated.spring(this.state.position, {
+			toValue: { x: 0, y: 0 }
+		}).start();
+	}
+
+	getCardStyle() {
+		const { position } = this.state;
+		const rotate = position.x.interpolate({
+			inputRange: [ -SCREEN_WIDTH * 2.5, 0, SCREEN_WIDTH * 2.5 ],
+			outputRange: [ '-120deg', '0deg', '120deg' ]
+		});
+
+		return {
+			...position.getLayout(),
+			transform: [ { rotate } ]
+		};
+	}
 
 	renderCards() {
 		return this.props.data.map((item, index) => {
 			if (index === 0) {
 				return (
-					<Animated.View
-						key={item.id}
-						style={this.state.position.getLayout()}
-						{...this.state.panResponder.panHandlers}
-					>
+					<Animated.View key={item.id} style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
 						{this.props.renderCard(item)}
 					</Animated.View>
 				);
