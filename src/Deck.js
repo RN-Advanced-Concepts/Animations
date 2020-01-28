f43858fa1e7ca3825f35a6b5e5514d7fe89d815b
fotis tsakiris <footios76@gmail.com>
2020-01-28 10:19:09 +0200

    swipe card and bring next one to top

--- src/Deck.js
+++ src/Deck.js
@@ -6,10 +6,13 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
-    static defaultProps = {
-        onSwipeRight: () => {},
-        onSwipeLeft: () => {}
-      }
+	// Whenever Deck is created, it will look at the props it is provided
+	// and if it is not provided with any of the props that are inside of this
+	// defaultProps obj it will automatically assign these ones.
+	static defaultProps = {
+		onSwipeRight: () => {},
+		onSwipeLeft: () => {}
+	};
 
 	constructor(props) {
 		super(props);
@@ -21,32 +24,32 @@ class Deck extends Component {
 			// If user drags the card
 			onPanResponderMove: (event, gesture) => {
 				// debugger
-                // console.log('gesture', gesture);
-                // move the card according to the move of the finger
+				// console.log('gesture', gesture);
+				// move the card according to the move of the finger
 				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
 			onPanResponderRelease: (event, gesture) => {
 				if (gesture.dx > SWIPE_THRESHOLD) {
-					console.log('gesture.dx swipe right', gesture.dx);
+					// console.log('gesture.dx swipe right', gesture.dx);
 					this.forceSwipe('right');
 				} else if (gesture.dx < -SWIPE_THRESHOLD) {
-                    console.log('gesture.dx swipe left', gesture.dx);
+					// console.log('gesture.dx swipe left', gesture.dx);
 					this.forceSwipe('left');
 				} else {
-					console.log('gesture.dx back to default', gesture.dx);
+					// console.log('gesture.dx back to default', gesture.dx);
 					this.resetPosition();
 				}
 			}
 		});
 
-        // panResponder does not call the state system
+		// panResponder does not call the state system
 		this.state = { panResponder, position, index: 0 };
 	}
 
 	forceSwipe(direction) {
-        const x = direction === 'right' ? SCREEN_WIDTH + 100 : -SCREEN_WIDTH - 100
-        // timing is more simple than spring. 
-        // It just moves the item.
+		const x = direction === 'right' ? SCREEN_WIDTH + 100 : -SCREEN_WIDTH - 100;
+		// timing is more simple than spring.
+		// It just moves the item.
 		Animated.timing(this.state.position, {
 			toValue: { x, y: 0 },
 			duration: SWIPE_OUT_DURATION
@@ -54,10 +57,12 @@ class Deck extends Component {
 	}
 
 	onSwipeComplete(direction) {
-        const { onSwipeLeft, onSwipeRight, data } = this.props;
-        const item = data[this.state.index]
+		const { onSwipeLeft, onSwipeRight, data } = this.props;
+		const item = data[this.state.index];
 
-		direction = 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        direction = 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({x: 0, y: 0});
+		this.setState({ index: this.state.index + 1 });
 	}
 
 	resetPosition() {
@@ -69,34 +74,39 @@ class Deck extends Component {
 	getCardStyle() {
 		const { position } = this.state;
 		const rotate = position.x.interpolate({
-            // * 2.5 = ... to have less rotation, because it take more distance to get to 120deg
+			// * 2.5 = ... to have less rotation, because it take more distance to get to 120deg
 			inputRange: [ -SCREEN_WIDTH * 2.5, 0, SCREEN_WIDTH * 2.5 ],
 			outputRange: [ '-120deg', '0deg', '120deg' ]
 		});
 
 		return {
-            // ...position.getLayout() =
-            // reference the position object, get the current layout, 
-            // and pass it to the Animated.View, which takes it over from there 
-            // Note: here we need the spead operator, because we're returning a single objece 
-            // return {}
-            // so we need to add all the properties of getLayout to that object.
+			// ...position.getLayout() =
+			// reference the position object, get the current layout,
+			// and pass it to the Animated.View, which takes it over from there
+			// Note: here we need the spead operator, because we're returning a single objece
+			// return {}
+			// so we need to add all the properties of getLayout to that object.
 			...position.getLayout(),
 			transform: [ { rotate } ]
 		};
 	}
 
 	renderCards() {
-		return this.props.data.map((item, index) => {
-			if (index === 0) {
+		return this.props.data.map((item, i) => {
+			// If these cards had already been swiped return null.
+			if (i < this.state.index) { return null; }
+
+			// Make a card and attach panHandlers
+			if (i === this.state.index) {
 				return (
-                    // {...this.state.panResponder.panHandlers} connects PanResponder to the View
-                    // style={this.props.position.getLayout()} // No spread operator needed!!!
+					// {...this.state.panResponder.panHandlers} connects PanResponder to the View
+					// style={this.props.position.getLayout()} // No spread operator needed!!!
 					<Animated.View key={item.id} style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
 						{this.props.renderCard(item)}
 					</Animated.View>
 				);
 			}
+			// else just make a card.
 			return this.props.renderCard(item);
 		});
 	}
